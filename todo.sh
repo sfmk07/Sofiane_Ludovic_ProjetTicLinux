1a0e7d46ae4a1e8866787641f67064b3dc2019da
Ludovic <ludovic.crousse@gmail.com>
2023-11-23 07:44:06 -0800

    Début des choix

---
+++ todo.sh
@@ -0,0 +1,15 @@
+#/!bin/bash  
+echo "Bienvenue dans le gestionnaire des tâches"!
+
+while true; do
+    echo "1. Ajouter une tâche"   
+    echo "2. Supprimer une tâche"    
+    echo "3. Affichier une tâche"    
+    echo "4. Quittez"      
+
+
+
+read -p "Choissisez une option (1/2/3/4)" choix
+
+case $choix 
+done
